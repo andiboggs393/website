8972220c4f653a3fdf0fa71cef2e0d56694063fa
Miles Exner <dizolivemint@gmail.com>
2019-03-23 16:57:01 -0700

    [Add] GET request

--- src/views/Markdown.vue
+++ src/views/Markdown.vue
@@ -1,18 +1,41 @@
 <template>
-    <!-- set options via props -->
-    <VueShowdown
-    markdown="## markdown text"
-    flavor="github"
-    :options="{ emoji: true }"/>
-
+    <div class="markdown">
+        <section class="page-section" id="markdown">
+            <div class="container">
+                <VueShowdown
+                :markdown='markdown'
+                flavor="github"
+                :options="{ emoji: true }"/> 
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
-import VueShowdown from 'vue-showdown'
+import { VueShowdown } from 'vue-showdown'
 
 export default {
+    name: 'markdown',
     components: {
         VueShowdown
+    },
+    data: () => ({
+        markdown: 'text'
+    }),
+    created () {
+        this.request("https://raw.githubusercontent.com/VueSD/website/master/README.md")
+    },
+    methods: {
+        request(url) {
+            var self = this
+            fetch(url)
+                .then(res => {
+                    return res.text()
+                }).then(text => {
+                    console.log(text)
+                    self.markdown = text
+                })
+        }
     }
 }
 </script>
