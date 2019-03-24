1bbb49c15cd0f8b84826d3da315cee7d0c154d99
Miles Exner <dizolivemint@gmail.com>
2019-03-24 13:00:35 -0700

    [Add] Resources page

---
+++ src/views/Resources.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="resources">
+        <VsdMarkdown :url="url" />
+    </div>
+</template>
+
+<script>
+import VsdMarkdown from '@/components/helpers/Markdown'
+
+export default {
+    name: 'resources',
+    components: {
+        VsdMarkdown
+    },
+    data: () => ({
+        url: "https://raw.githubusercontent.com/VueSD/website/master/README.md"
+    })
+}
+</script>
