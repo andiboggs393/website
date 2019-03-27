3c5a112566de575f3f3b85f15f172db80a74a753
Miles Exner <dizolivemint@gmail.com>
2019-03-26 20:17:03 -0700

    [Add] Markdown and HTML webpack loader

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,19 @@ module.exports = {
         data: `@import "@/assets/scss/_shared.scss";`
       }
     }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('markdown-loader')
+      .test(/\.md$/)
+      .use('html')
+        .loader('html-loader')
+        .end()
+      .use('markdown')
+        .loader('markdown-loader')
+        .options({
+          gfm: true
+        })
+        .end()
   }
 };
