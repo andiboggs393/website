5ce6e2b10b7512f87be5b26a0f2040f22b984e9a
Miles Exner <dizolivemint@gmail.com>
2019-03-27 17:07:21 -0700

    [Update] Changed prop from url to markdown

--- src/components/helpers/Markdown.vue
+++ src/components/helpers/Markdown.vue
@@ -17,21 +17,12 @@ import { VueShowdown } from 'vue-showdown'
 export default {
     name: 'vsd-markdown',
     props: {
-        url: {
+        markdown: {
             type: String
         }
     },
     components: {
         VueShowdown
-    },
-    data: () => ({
-        markdown: ''
-    }),
-    async mounted () {
-        this.markdown = await fetch(this.url).then(res => res.text()).catch(err => {
-            return `# Unable to fetch
-                    ${err}`
-        })
     }
 }
 </script>
