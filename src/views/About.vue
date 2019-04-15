c92a0774b970135d28213c5cd2950668170fc83f
Austin Gil <austin@stegosource.com>
2019-04-14 19:36:30 -0700

    Filling out content for the About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+  <main class="container pt-5">
+    <header>
+      <h1>This is an about page</h1>
+    </header>
 
-<style>
-.about {
-  padding-top: 100px;
-}
-</style>
+    <p>
+      VueSD is a part of San Diego JavaScript: a registered 501(c)(3) nonprofit
+      whose mission is to cultivate and grow the JavaScript community in and
+      around San Diego.
+    </p>
+
+    <p>
+      You can find us online at
+      <a href="https://sandiegojs.org">sandiegojs.org</a>,
+      <a href="https://www.meetup.com/sandiegojs/">meetup.com/sandiegojs</a>,
+      <a href="https://twitter.com/sandiegojs">twitter.com/sandiegojs</a>, and
+      <a href="https://github.com/sandiegojs/">github.com/sandiegojs</a>.
+    </p>
+
+    <p>
+      To support our organization with tax-deductible donations, please visit
+      <a href="https://www.patreon.com/sdjs">patreon.com/sdjs</a>.
+    </p>
+  </main>
+</template>
